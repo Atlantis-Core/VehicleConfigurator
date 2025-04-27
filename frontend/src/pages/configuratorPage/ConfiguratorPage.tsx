c2eaf7c30dc18769abf661d45bd99ebfd4bb1f26
David Pospisil <pospisil.david.29@gmail.com>
2025-04-27 12:28:07 +0200

    Created first prototype for 3d model viewer

--- frontend/src/pages/configuratorPage/ConfiguratorPage.tsx
+++ frontend/src/pages/configuratorPage/ConfiguratorPage.tsx
@@ -1,10 +1,12 @@
 import styles from "./ConfiguratorPage.module.css";
+import VehicleViewer from "@components/VehicleViewer";
 
 const ConfiguratorPage = () => {
 
   return (
     <div>
       <p>Configurator Page</p>
+      <VehicleViewer />
     </div>
   )
 }
