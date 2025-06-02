a6419e7cd0c46ec893c9bbd6649f8a9fa87c694b
David Pospisil <pospisil.david.29@gmail.com>
2025-06-02 14:45:18 +0200

    Update .gitignore, remove static.yaml, and add base path to vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,24 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@components': path.resolve(__dirname, './src/components'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@hooks': path.resolve(__dirname, './src/hooks'),
-      '@lib': path.resolve(__dirname, './src/lib'),
-      '@api': path.resolve(__dirname, './src/api'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@globals': path.resolve(__dirname, './src/globals'),
-      '@context': path.resolve(__dirname, './src/context'),
-      '@utils': path.resolve(__dirname, './src/utils'),
-      '@types': path.resolve(__dirname, './src/types'),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@lib": path.resolve(__dirname, "./src/lib"),
+      "@api": path.resolve(__dirname, "./src/api"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@globals": path.resolve(__dirname, "./src/globals"),
+      "@context": path.resolve(__dirname, "./src/context"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+      "@types": path.resolve(__dirname, "./src/types"),
     },
   },
   server: {
     port: 3000,
-  }
-})
+  },
+  preview: {
+    port: 3000,
+  },
+  base: "/",
+});
